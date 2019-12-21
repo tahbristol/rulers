6edbe303c5ee3908f1fd7122dbfdaff29807a2c2
tahbristol <tahbristol@gmail.com>
2019-12-20 20:35:00 -0500

    controller

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -4,9 +4,22 @@ require "rulers/array"
 module Rulers
   class Application
     def call(env)
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
       `echo debug > debug.txt`;
       [200, {'Content-Type' => 'text/html'},
-      ["Hello from Ruby on Rulers!"]]
+      [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
--- rulers.gemspec
+++ rulers.gemspec
@@ -33,4 +33,5 @@ Gem::Specification.new do |spec|
   spec.add_runtime_dependency "rack"
   spec.add_development_dependency "rack-test"
   spec.add_development_dependency "test-unit"
+  spec.add_development_dependency "unicorn"
 end
